aeb14b690f370b7b3378225e15daef86632f6194
Stariu0013 <vladstarynets@ukr.net>
2021-03-08 22:33:25 +0200

    :--- added types

--- my-app/src/components/Users/Users.tsx
+++ my-app/src/components/Users/Users.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import Paginator from "../common/Pagination/Paginator";
 import User from "./User";
+import {UserType} from "../../types/types";
 
-const Users = props => {
+type TUsers = {
+    currentPage: number,
+    users: UserType[],
+    followingInProgress: number[],
+    pageSize: number,
+    totalItemsCount: number,
+
+    onPageChanged: (p: number) => void,
+    follow: (id: number) => void,
+    unfollow: (id: number) => void,
+}
+
+const Users: React.FC<TUsers> = (props) => {
 
     return <div>
-        <Paginator {...props} currentPage={props.currentPage} onPageChanged={props.onPageChanged} portionSize={5}/>
+        <Paginator {...props} pageSize={props.pageSize} totalItemsCount={props.totalItemsCount}
+                   currentPage={props.currentPage} onPageChanged={props.onPageChanged} portionSize={5}/>
         {
-            props.users.map(u => <User follow={props.follow}
+            props.users.map((u: UserType) => <User follow={props.follow}
                                        followingInProgress={props.followingInProgress}
                                        unfollow={props.unfollow}
                                        user={u}
