004c21fa87829d54544fab46e5778d26aa7557c6
Stariu0013 <vladstarynets@ukr.net>
2021-03-15 21:17:17 +0200

    :---- refactored code

--- my-app/src/components/Profile/Profile.jsx
+++ my-app/src/components/Profile/Profile.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
+import {updateProfile} from "../../redux/profile-reducer";
 
 class Profile extends React.Component {
 
     render = () => {
         return (
             <div>
-                <ProfileInfo profile={this.props.profile} status={this.props.status} updateStatus={this.props.updateStatus}/>
+                <ProfileInfo profile={this.props.profile}
+                             updateProfile={this.props.updateProfile}
+                             isOwner={this.props.isOwner}
+                             savePhoto={this.props.savePhoto}
+                             status={this.props.status}
+                             updateStatus={this.props.updateStatus}/>
                 <MyPostsContainer />
             </div>
         )
