8b719e2f70fd6f65e510a25db2b4399f36dfd016
Stariu0013 <vladstarynets@ukr.net>
2021-03-16 00:35:02 +0200

    :---- added types

--- my-app/src/utils/validators/validator.ts
+++ my-app/src/utils/validators/validator.ts
@@ -1,7 +1,9 @@
-export const requiredField = value => {
+export type TValidators = (value: string) => string | void;
+
+export const requiredField: TValidators = value => {
     return !value ? 'Filed is required' : undefined;
 };
 
-export const fieldMaxLength = maxLength => value => {
+export const fieldMaxLength = (maxLength: number): TValidators => value => {
     return value.length > maxLength ? `Max length is ${maxLength} symbols` : undefined;
 };
