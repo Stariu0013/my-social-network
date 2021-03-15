a18059d128f6aea0d0ac241421b1f2cd50cec840
Stariu0013 <vladstarynets@ukr.net>
2021-03-15 21:24:01 +0200

    :---- added aboutMe for TProfile

--- my-app/src/types/types.ts
+++ my-app/src/types/types.ts
@@ -1,29 +1,30 @@
 type TContacts = {
-    github: string | null
-    vk: string | null
-    facebook: string | null
-    instagram: string | null
-    twitter: string | null
-    website: string | null
-    youtube: string | null
-    mainLink: string | null
+    github: string | null,
+    vk: string | null,
+    facebook: string | null,
+    instagram: string | null,
+    twitter: string | null,
+    website: string | null,
+    youtube: string | null,
+    mainLink: string | null,
 };
-export type TPhotoes = {
-    large: string | null
-    small: string | null
+export type TPhotos = {
+    large: string | null,
+    small: string | null,
 };
 export type TProfile = {
-    userId: number
-    lookingForAJob: boolean
-    lookingForAJobDescription: string
-    fullName: string
-    contacts: TContacts
-    photos: TPhotoes
+    userId: number,
+    lookingForAJob: boolean,
+    lookingForAJobDescription: string,
+    aboutMe: string,
+    fullName: string,
+    contacts: TContacts,
+    photos: TPhotos,
 };
 export type TUser = {
     id: number,
     name: string,
     status: string,
-    photos: TPhotoes,
+    photos: TPhotos,
     followed: boolean,
-}
+};
