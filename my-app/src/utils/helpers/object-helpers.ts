650218b198c0b8838c4134b05b7dc67305387dbe
Stariu0013 <vladstarynets@ukr.net>
2021-06-04 14:14:04 +0300

    ref: added types

--- my-app/src/utils/helpers/object-helpers.ts
+++ my-app/src/utils/helpers/object-helpers.ts
@@ -1,5 +1,5 @@
-export const updateObjectInArray = (items, objPropName, itemId, newObjProps) => {
-    return items.map(u => {
+export const updateObjectInArray = (items: any, objPropName: any, itemId: any, newObjProps: any) => {
+    return items.map((u: any) => {
         if (u[objPropName] === itemId) {
             return {...u, ...newObjProps}
         }
