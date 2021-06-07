ff4f39fb3831b5ed414bce8d4f2d10dea932447e
Stariu0013 <vladstarynets@ukr.net>
2021-06-07 21:55:14 +0300

    ref: instead of using connect HOC i use selector hooks and dispatch hook

--- my-app/src/components/Users/Users.tsx
+++ my-app/src/components/Users/Users.tsx
@@ -1,35 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Paginator from "../common/Pagination/Paginator";
 import User from "./User";
-import {TUser} from "../../types/types";
-import {UsersSearchForm} from "./UsersSearchForm";
-import {TFilter} from "../../redux/users-reducer";
+import { TUser } from "../../types/types";
+import { UsersSearchForm } from "./UsersSearchForm";
+import { actions, follow, requestUsers, TFilter, unfollow } from "../../redux/users-reducer";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    getCurrentPage,
+    getFilter,
+    getFollowingInProgress,
+    getPageSize,
+    getUsers,
+    getUsersTotalCount,
+} from "../../selectors/user-selector";
 
-type TUsers = {
-    currentPage: number,
-    users: TUser[],
-    followingInProgress: number[],
-    pageSize: number,
-    totalItemsCount: number,
+const Users: React.FC = (props) => {
+    const pageSize = useSelector(getPageSize);
+    const totalItemsCount = useSelector(getUsersTotalCount);
+    const currentPage = useSelector(getCurrentPage);
+    const users = useSelector(getUsers);
+    const followingInProgress = useSelector(getFollowingInProgress);
+    const filter = useSelector(getFilter);
+    const dispatch = useDispatch();
 
-    onPageChanged: (p: number) => void,
-    follow: (id: number) => void,
-    unfollow: (id: number) => void,
-    onFilterChange: (filter: TFilter) => void,
-}
+    useEffect(() => {
+        dispatch(requestUsers(currentPage, pageSize, filter));
+    }, []);
 
-const Users: React.FC<TUsers> = (props) => {
+    const followUser = (id: number) => {
+        dispatch(follow(id));
+    };
+    const unfollowUser = (id: number) => {
+        dispatch(unfollow(id));
+    };
+    const onPageChanged = (pageNumber: number) => {
+        dispatch(requestUsers(pageNumber, pageSize, filter));
+    };
+    const onFilterChange = (filter: TFilter) => {
+        dispatch(actions.setFilter(filter));
+        dispatch(requestUsers(1, pageSize, filter));
+    };
 
     return <div>
+        <UsersSearchForm onFilterChange={onFilterChange}/>
 
-        <UsersSearchForm onFilterChange={props.onFilterChange}/>
-
-        <Paginator {...props} pageSize={props.pageSize} totalItemsCount={props.totalItemsCount}
-                   currentPage={props.currentPage} onPageChanged={props.onPageChanged} portionSize={5}/>
+        <Paginator {...props} pageSize={pageSize} totalItemsCount={totalItemsCount}
+                   currentPage={currentPage} onPageChanged={onPageChanged} portionSize={5}/>
         {
-            props.users.map((u: TUser) => <User follow={props.follow}
-                                                followingInProgress={props.followingInProgress}
-                                                unfollow={props.unfollow}
+            users.map((u: TUser) => <User follow={followUser}
+                                                followingInProgress={followingInProgress}
+                                                unfollow={unfollowUser}
                                                 user={u}
                                                 key={u.id}
             />)
